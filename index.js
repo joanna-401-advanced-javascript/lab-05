f3fd0a676515a6bef8fc372169b0431c707b0f9d
joannaarroyo <joliuac@gmail.com>
2019-08-16 20:43:48 -0700

    added info to run from index.js

--- index.js
+++ index.js
@@ -3,14 +3,43 @@
 const mongoose = require('mongoose');
 
 // Require your model
+const Categories = require('./models-singular/categories');
 
 // Mongoose Server URI
-const MONGOOSE_URI = 'mongodb://localhost:27017/class05';
+const MONGOOSE_URI = 'mongodb://localhost/lab-05';
 
 // Connect
-mongoose.connect(...);
+mongoose.connect(MONGOOSE_URI, {useNewUrlParser: true} );
 
 // Do some work
+const categories = new Categories;
+
+const createdCategory = categories.create({
+  name: 'dandelion',
+  description: 'floaty fur ball'
+})
+  .then(savedCategory => {
+    console.log('SAVED', savedCategory)
+    return categories.create({
+      name: 'seal',
+      description: 'sea puppy'
+    });
+  })
+  .then(savedCategory => {
+    console.log('SAVED AGAIN', savedCategory);
+    return categories.get();
+  })
+  .then(allCategories => {
+    console.log('ALL CATEGORIES', allCategories);
+    return categories.update(allCategories.results[0]._id, {
+      name: 'pizza',
+      description: 'happiness on a plate'
+    });
+  })
+  .then(updatedCategory => {
+    console.log('UPDATED', updatedCategory);
+  })
+  .catch(console.log);
 
 // Disconnect
-mongoose.disconnect();
+// mongoose.disconnect();
